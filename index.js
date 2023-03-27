88ce7ddb37d8b493738d2ea2d6b6f07b272b5296
waltersimekha <waltersimekha@gmail.com>
2023-03-27 09:12:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+function superBowlWin=record.find(function(game){
+  return game.result==="W";
+});
+if (winningYear) {
+  return winningYear.year;
+}
+else{
+  return undefined;
+}
